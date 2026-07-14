50a0aa5682e7681c3a9ab64ebf41e692b6351af7
dfn-slxxp <waldman.sebastian@gmail.com>
2026-07-14 16:16:36 -0400

    scripts (and building)

---
+++ Scripts/linux.bat
@@ -0,0 +1,37 @@
+@echo off
+setlocal EnableExtensions
+
+set "version_number=v1.3.2"
+set "modpack_name=Wildcats Mod"
+set "folder_name=%modpack_name%"
+set "dll_name=Wildcats.dll"
+set "zip_name=Wildcats Release"
+
+set "script_dir=%~dp0"
+for %%I in ("%script_dir%..") do set "repo_root=%%~fI"
+
+set "mods_dir=%repo_root%\Mods"
+set "scripts_dir=%script_dir%"
+set "library_dir=%repo_root%\Library"
+
+set "source_mod_folder=%mods_dir%\%folder_name%"
+set "target_mod_folder=%scripts_dir%\%folder_name%"
+
+set "catalog_hash_source=%library_dir%\com.unity.addressables\aa\Linux\catalog.hash"
+set "catalog_json_source=%library_dir%\com.unity.addressables\aa\Linux\catalog.json"
+set "settings_json_source=%library_dir%\com.unity.addressables\aa\Linux\settings.json"
+set "dll_source=%library_dir%\ScriptAssemblies\%dll_name%"
+
+set "initial_archive_name=%folder_name%.zip"
+set "final_archive_name=%zip_name% %version_number% Linux.zip"
+
+move /Y "%source_mod_folder%" "%target_mod_folder%"
+move /Y "%catalog_hash_source%" "%target_mod_folder%"
+move /Y "%catalog_json_source%" "%target_mod_folder%"
+move /Y "%settings_json_source%" "%target_mod_folder%"
+move /Y "%dll_source%" "%target_mod_folder%"
+
+powershell -NoProfile -Command "Compress-Archive -Path '%scripts_dir%%folder_name%' -DestinationPath '%scripts_dir%%initial_archive_name%' -Force"
+move /Y "%scripts_dir%%initial_archive_name%" "%scripts_dir%%final_archive_name%"
+move /Y "%scripts_dir%%final_archive_name%" "%mods_dir%"
+rmdir /S /Q "%target_mod_folder%"
