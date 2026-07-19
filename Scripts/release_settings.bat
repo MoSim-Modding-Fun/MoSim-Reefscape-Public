84baa4aa9cb4e2b5dedab837a02c1bc9e91b93ac
dfn-slxxp <waldman.sebastian@gmail.com>
2026-07-19 17:38:28 -0400

    buiklding

--- Scripts/release_settings.bat
+++ Scripts/release_settings.bat
@@ -1,5 +1,5 @@
 set "version_number=v1.0.0"
-set "modpack_name="Lanternfly"
+set "modpack_name=Lanternfly Mod"
 set "folder_name=%modpack_name%"
 set "dll_name=Lanternfly.dll"
 set "zip_name=Lanternfly Release"
