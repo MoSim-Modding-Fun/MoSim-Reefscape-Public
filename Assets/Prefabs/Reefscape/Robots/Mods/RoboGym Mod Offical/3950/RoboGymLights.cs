d68f8bafe146f020d04b0fff3c86b1b756110a10
darksplice <darksplice0@proton.me>
2026-07-22 20:40:31 -0400

    LEDS

---
+++ Assets/Prefabs/Reefscape/Robots/Mods/RoboGym Mod Offical/3950/RoboGymLights.cs
@@ -0,0 +1,75 @@
+using Games.Reefscape.Enums;
+using Games.Reefscape.GamePieceSystem;
+using Games.Reefscape.Robots;
+using RobotFramework.Components;
+using RobotFramework.Controllers.GamePieceSystem;
+using UnityEngine;
+
+namespace Prefabs.Reefscape.Robots.Mods.RoboGym._3950
+{
+    public class RoboGymLights : MonoBehaviour
+    {
+        [Header("LED Strips")]
+        public GameObject[] strips;
+
+        [Header("Textures")]
+        public Shader shaderGraphShader;
+        public Texture red;
+        public Texture green;
+        public Texture white;
+
+        [SerializeField] private GenericElevator elevator;
+
+        private ReefscapeRobotBase _base;
+        private RobotGamePieceController<ReefscapeGamePiece, ReefscapeGamePieceData>.GamePieceControllerNode _coralController;
+        private Material _material;
+
+        private void Start()
+        {
+            _base = GetComponent<ReefscapeRobotBase>();
+            _coralController = GetComponent<RobotGamePieceController<ReefscapeGamePiece, ReefscapeGamePieceData>>()
+                ?.GetPieceByName(ReefscapeGamePieceType.Coral.ToString());
+
+            _material = new Material(shaderGraphShader);
+            foreach (var strip in strips)
+            {
+                strip.GetComponentInChildren<Renderer>().material = _material;
+            }
+        }
+
+        private void Update()
+        {
+            if (_base == null || _coralController == null) return;
+
+            bool coral = _coralController.HasPiece();
+            bool elevatorUp = elevator != null && elevator.GetElevatorHeight() > 1f;
+            bool aligning = _base.AutoAlignLeftAction.IsPressed() || _base.AutoAlignRightAction.IsPressed();
+
+            if (aligning)
+            {
+                Set(white, Blink(6f));
+            }
+            else if (coral)
+            {
+                Set(green, elevatorUp ? Blink(5f) : 20f);
+            }
+            else
+            {
+                Set(red, 20f);
+            }
+        }
+
+        private static float Blink(float hz)
+        {
+            return Time.time % (1f / hz) > 1f / (hz * 2f) ? 20f : 0f;
+        }
+
+        private void Set(Texture texture, float intensity)
+        {
+            _material.SetFloat("_X", 0f);
+            _material.SetFloat("_Y", 0f);
+            _material.SetFloat("_intensity", intensity);
+            if (texture != null) _material.SetTexture("_Texture2D", texture);
+        }
+    }
+}
